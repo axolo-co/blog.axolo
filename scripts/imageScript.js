2b8467ec0a2c6620bb1f52a673d52e2e0652b22a
Sydney Cohen <chnsydney@gmail.com>
2025-08-28 10:38:56 +0200

    code review tools

--- scripts/imageScript.js
+++ scripts/imageScript.js
@@ -1,6 +1,6 @@
 // Here put the folder name and it will create a component for each image that he can
 
-const folder = "public/static/images/code-review/"
+const folder = "public/static/images/code-review-best-practices/"
 
 var sizeOf = require("image-size")
 var fs = require("fs")
